5ec7f5a42e4efb7509f379404621193dc0de03d2
Kat Maldon <katharinamaldon@gmail.com>
2019-03-28 15:55:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,7 +17,9 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end 
 
-
+def now_serving(katz_deli)
+  counter = 0 
+  puts ""
   
   
   
