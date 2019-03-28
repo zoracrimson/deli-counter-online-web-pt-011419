99844c4597f19df4914b48cd0b6e9586046e2aa6
Kat Maldon <katharinamaldon@gmail.com>
2019-03-28 15:44:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ katz_deli = []
 
 def line(katz_deli)
   phrase = "The line is currently:"
-  if katz_deli > 0 
+  if katz_deli.size > 0 
     katz_deli.each_with_index do | name, index |
       puts "#{phrase} + #{index+1}. + #{name}."
     end 
