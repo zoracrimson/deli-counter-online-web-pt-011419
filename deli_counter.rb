03f8e92be48a31e1357836df54a75bd128ea729b
Kat Maldon <katharinamaldon@gmail.com>
2019-03-28 15:45:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli)
   phrase = "The line is currently:"
   if katz_deli.size > 0 
     katz_deli.each_with_index do | name, index |
-      puts "#{phrase} + #{index+1}. + #{name}."
+      puts "#{phrase} #{index+1}. #{name}."
     end 
   else 
     puts "The line is currently empty."
